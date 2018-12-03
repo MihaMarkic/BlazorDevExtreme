5cb1b845a56fb5336e377fdba616afdcb30f696d
Geert Deltour <geert.deltour@telenetgroup.be>
2018-12-03 08:28:46 +0100

    Added datagrid and scheduler examples (with services)

---
+++ sources/BlazorDevExtremeApp/Objects/SchedulerData.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorDevExtremeApp.Objects
+{
+    public class SchedulerData
+    {
+        public String text { get; set; }
+        public DateTime startDate { get; set; }
+        public DateTime endDate { get; set; }
+
+        public Boolean allDay { get; set; }
+
+        public SchedulerData(String subject, DateTime from, DateTime to, Boolean day)
+        {
+            text = subject;
+            startDate = from;
+            endDate = to;
+            allDay = day;
+
+        }
+    }
+}
